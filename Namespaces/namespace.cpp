47a6951e0f8de044f14b97f433e170e50df4ef23
mohsin0176 <mohsin2010ju@gmail.com>
2021-01-02 20:33:58 +0600

    namespace    code created

---
+++ Namespaces/namespace.cpp
@@ -0,0 +1,31 @@
+#include<iostream>
+using namespace std;
+int main()
+
+{
+    namespace Electronics
+    {
+        int TotalStock;
+        class Headphones
+        {
+
+        };
+        class Television
+        {
+
+        };
+    }
+    namespace Shoes
+    {
+        int TotalStock;
+        class Sandal
+        {
+
+        };
+        class Slipper
+        {
+
+        };
+    }
+    return 0;
+}
