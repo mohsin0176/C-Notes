aafbf49a5b4a42e21ed3f06d71897c3af14179d2
mohsin0176 <mohsin2010ju@gmail.com>
2021-01-02 13:00:42 +0600

    file created

--- Access Modifier/private1.cpp
+++ Access Modifier/private1.cpp
@@ -1,1 +1,22 @@
+#include<iostream>
+using namespace std;
+class Rectangle
+{
+private:
+    int length,breadth;
+public:
+    int area(int l,int b)
+    {
+        length=l;
+        breadth=b;
+        int area=length*breadth;
+        cout<<area;
+    }
+};
 
+int main()
+{
+    Rectangle rect1;
+    rect1.area(10,20);
+    return 0;
+}
--- Access Modifier/protected.cpp
+++ Access Modifier/protected.cpp
@@ -1,1 +1,28 @@
+#include<iostream>
+using namespace std;
 
+class Rectangle
+{
+protected:
+    int height;
+
+};
+class Square:public Rectangle
+{
+public:
+    void setHeight(int h)
+    {
+        height=h;
+    }
+    void displayHeight()
+    {
+        cout<<height<<endl;
+    }
+};
+int main()
+{
+    Square square1;
+    square1.setHeight(10);
+    square1.displayHeight();
+    return 0;
+}
