2b22b3b35de6c8f38c5823e91f1393459b9a605e
mohsin0176 <mohsin2010ju@gmail.com>
2021-01-02 14:45:21 +0600

    file created

---
+++ Array/multi.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int i,j,n,m;
+    cin>>n>>m;
+    int arr[n][m];
+
+    for(i=0;i<=n;i++)
+    {
+        for(j=0;j<=m;j++)
+        {
+            cin>>arr[i][j];
+            cout<<"\n";
+        }
+    }
+        for(i=0;i<=n;i++)
+    {
+        for(j=0;j<=m;j++)
+        {
+            cout<<arr[i][j];
+            cout<<"\t";
+        }
+         cout<<"\n";
+    }
+    return 0;
+}
