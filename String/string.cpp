60fb08c2997da670dbeade5a9474c954436d0264
mohsin0176 <mohsin2010ju@gmail.com>
2021-01-02 17:35:02 +0600

    string code created

---
+++ String/string.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    char ch[]={'C','E','O'};
+    string s1="Hello";
+    string s2=string(ch);
+    cout<<s1<<endl<<s2<<endl;
+    return 0;
+}
