b8f9f6c24f74682781ff0a25269a0120fbf16744
mohsin0176 <mohsin2010ju@gmail.com>
2021-01-03 21:29:19 +0600

    c++ programme

--- Pattern Programme/PP-02.cpp
+++ Pattern Programme/PP-02.cpp
@@ -1,1 +1,17 @@
 
+#include<iostream>
+using namespace std;
+int main()
+{
+int i,j;
+    for(i=5;i>=0;i--)
+    {
+        for(j=0;j<=i;j++)
+        {
+            cout<<"*";
+        }
+        cout<<endl;
+    }
+
+    return 0;
+}
