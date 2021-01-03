0bf17e728a3fad8378d83368e16de87a1fa11014
mohsin0176 <mohsin2010ju@gmail.com>
2021-01-03 21:02:42 +0600

    c++ programme

--- Basic Programme/concat-string-object.cpp
+++ Basic Programme/concat-string-object.cpp
@@ -1,1 +1,13 @@
-
+#include<iostream>
+#include<cstring>
+using namespace std;
+int main()
+{
+    char s1[15],s2[15],s3[150];
+    cin>>s1;
+    cin>>s2;
+    strcat(s1,s2);
+    strcpy(s3,s1);
+    cout<<s3;
+    return 0;
+}
--- Basic Programme/length-of-string-object.cpp
+++ Basic Programme/length-of-string-object.cpp
@@ -1,1 +1,8 @@
-
+#include<iostream>
+using namespace std;
+int main()
+{
+    string ch="C++ programming";
+    cout<<ch.size();
+    return 0;
+}
