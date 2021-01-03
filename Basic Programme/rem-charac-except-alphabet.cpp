911c2a928123f7259e045b6d5abf21439147334f
mohsin0176 <mohsin2010ju@gmail.com>
2021-01-03 21:22:57 +0600

    c++ programme

--- Basic Programme/rem-charac-except-alphabet.cpp
+++ Basic Programme/rem-charac-except-alphabet.cpp
@@ -1,1 +1,20 @@
 
+#include<iostream>
+using namespace std;
+int main()
+{
+    char line[100],alphabetstring[100];
+    int j=0;
+    cin.getline(line,100);
+    for(int i=0;line[i] !='\0';i++)
+    {
+        if((line[i] >='a' && line[i]<='z'))
+        {
+            alphabetstring[j++]=line[i];
+        }
+    }
+    alphabetstring[j]='\0';
+
+    cout<<alphabetstring;
+    return 0;
+}
