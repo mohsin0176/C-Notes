d518d1cc4f5855b0ff43127bc562ab93dc8aec1e
mohsin0176 <mohsin2010ju@gmail.com>
2021-01-03 19:02:37 +0600

    c++ programme

--- Basic Programme/vowel-constant.cpp
+++ Basic Programme/vowel-constant.cpp
@@ -1,1 +1,20 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    char c;
+    char low,up;
+    cin>>c;
+    low=(c='a'||c=='e'||c=='i'||c=='o'||c=='u');
+    up=(c=='A'||c=='E'||c=='I'||c=='O'||c=='U');
+    if(low||up)
+    {
+        cout<<"Vowel";
+    }
+    else
+    {
+        cout<<"Constant";
+    }
 
+    return 0;
+}
