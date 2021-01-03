819337d0e6db3122ab62ce1e40f932adc89f002d
mohsin0176 <mohsin2010ju@gmail.com>
2021-01-03 19:10:57 +0600

    c++ programme

--- Basic Programme/large-if.cpp
+++ Basic Programme/large-if.cpp
@@ -1,1 +1,20 @@
-
+#include<iostream>
+using namespace std;
+int main()
+{
+    int n1,n2,n3;
+    cin>>n1>>n2>>n3;
+    if(n1>n2&&n1>n3)
+    {
+        cout<<n1<<"is large";
+    }
+    else if(n2>n1&&n2>n1)
+    {
+        cout<<n2<<"is large";
+    }
+    else
+        {
+        cout<<n3<<"is large";
+        }
+    return 0;
+}
