1fa83ec6e4c22848965eac5e0b64f97dec0ef28f
mohsin0176 <mohsin2010ju@gmail.com>
2021-01-03 20:01:28 +0600

    c++ programme

--- Basic Programme/rev-int.cpp
+++ Basic Programme/rev-int.cpp
@@ -1,1 +1,18 @@
+#include<iostream>
+using namespace std;
 
+int main()
+{
+
+    int n,reverseNumber=0,remainder;
+    cin>>n;
+    while(n!=0)
+    {
+        remainder=n%10;
+        reverseNumber=reverseNumber*10+remainder;
+        n=n/10;
+    }
+    cout<<reverseNumber;
+
+    return 0;
+}
