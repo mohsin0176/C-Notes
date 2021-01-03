d56618363719f7bc18ebdcb6dff20fdf7440e858
mohsin0176 <mohsin2010ju@gmail.com>
2021-01-03 20:18:41 +0600

    c++ programme

--- Basic Programme/prime.cpp
+++ Basic Programme/prime.cpp
@@ -1,1 +1,21 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int i,n;
+    cin>>n;
+    for(i=2;i<=(n-1);i++)
+    {
+        if(n%i==0)
+        {
+            cout<<"Number is Not Prime";
+            break;
+        }
+        else
+        {
+            continue;
+        }
+    }
+    return 0;
+}
 
