b7647e19d63137acfa1ae7cfb42c9ae762e6df4f
mohsin0176 <mohsin2010ju@gmail.com>
2021-01-03 19:31:07 +0600

    c++ programme

--- Basic Programme/fact.cpp
+++ Basic Programme/fact.cpp
@@ -1,1 +1,13 @@
-
+#include<iostream>
+using namespace std;
+int main()
+{
+    int n,i,fact=1;
+    cin>>n;
+    for(i=n;i>=1;i--)
+    {
+        fact=fact*i;
+    }
+    cout<<fact;
+    return 0;
+}
