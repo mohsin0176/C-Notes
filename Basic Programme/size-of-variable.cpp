f0ffbfb1b65fd018900f3c8a9cc9125d328007df
mohsin0176 <mohsin2010ju@gmail.com>
2021-01-03 18:47:47 +0600

    c++ programme

--- Basic Programme/size-of-variable.cpp
+++ Basic Programme/size-of-variable.cpp
@@ -1,1 +1,12 @@
-
+#include<iostream>
+using namespace std;
+int main()
+{
+    cout<<sizeof(int)<<endl;
+    cout<<sizeof(char)<<endl;
+    cout<<sizeof(float)<<endl;
+    cout<<sizeof(double)<<endl;
+    cout<<sizeof(long)<<endl;
+    cout<<sizeof(string)<<endl;
+    return 0;
+}
