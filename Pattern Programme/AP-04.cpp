a9cbe67931f7b58fde5526df8e3a62d541d1b793
mohsin0176 <mohsin2010ju@gmail.com>
2021-01-04 00:30:00 +0600

    c++ programme

--- Pattern Programme/AP-04.cpp
+++ Pattern Programme/AP-04.cpp
@@ -1,1 +1,16 @@
-
+#include<iostream>
+using namespace std;
+int main()
+{
+    int i,j;
+    char s[]="INDIA";
+    for(i=0;s[i];i++)
+    {
+        for(j=0;j<=i;j++)
+        {
+            cout<<s[j];
+        }
+        cout<<endl;
+    }
+    return 0;
+}
--- Pattern Programme/AP-06.cpp
+++ Pattern Programme/AP-06.cpp
@@ -1,1 +1,21 @@
-
+#include<iostream>
+using namespace std;
+int main()
+{
+    int i,j,k,n=5;
+    char c='A';
+    for(i=0;i<=n;i++)
+    {
+        for(j=0;j<=i;j++)
+        {
+            cout<<" ";
+        }
+        for(k=n-i-1;k>=0;k--)
+        {
+            cout<<c;
+            c++;
+        }
+        cout<<endl;
+    }
+    return 0;
+}
