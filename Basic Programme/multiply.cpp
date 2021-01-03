9cc1f3a13d15c48272901ca17ddfde005ca68fe8
mohsin0176 <mohsin2010ju@gmail.com>
2021-01-03 19:35:42 +0600

    c++ programme

--- Basic Programme/multiply.cpp
+++ Basic Programme/multiply.cpp
@@ -1,1 +1,14 @@
 
+#include<iostream>
+using namespace std;
+int main()
+{
+    int mul,n,i;
+    cin>>n;
+    for(i=1;i<=10;i++)
+    {
+        mul=n*i;
+        cout<<"Result is :"<<mul<<endl;
+    }
+    return 0;
+}
