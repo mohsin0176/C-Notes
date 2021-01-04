0c1514dab65231dd11473c3ca1c82a8788af2a16
mohsin0176 <mohsin2010ju@gmail.com>
2021-01-04 23:01:27 +0600

    C++  codes

--- Algorithm/strong.cpp
+++ Algorithm/strong.cpp
@@ -1,1 +1,28 @@
-
+#include<iostream>
+using namespace std;
+int main()
+{
+    int ip,sum=0;
+    cin>>ip;
+    int save=ip;
+    while(ip)
+    {
+        int num=ip%10;
+        int fact=1;
+        for(int i=num;i>0;i--)
+        {
+            fact=fact*i;
+        }
+        sum+=fact;
+        ip/=10;
+    }
+    if(sum==save)
+    {
+        cout<<save<<"strong number";
+    }
+    else
+    {
+        cout<<save<<"not strong number";
+    }
+    return 0;
+}
