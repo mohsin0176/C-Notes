ec42937be3f24531cb29097fd343bab97186804d
mohsin0176 <mohsin2010ju@gmail.com>
2021-01-04 22:46:14 +0600

    C++  codes

--- Algorithm/number-integer-has-x-divisor.cpp
+++ Algorithm/number-integer-has-x-divisor.cpp
@@ -1,1 +1,32 @@
+#include<iostream>
+using namespace std;
+int noofDivisors(int num)
+{
+    int count=0;
+    for(int i=1;i<=num;i++)
+    {
+        if(num%i==0)
+        {
+            count++;
+        }
+    }
+    return count;
+}
 
+int main()
+{
+    int l_limit,u_limit,num,i;
+    cin>>l_limit>>u_limit;
+    cin>>num;
+    int count=0;
+    for(i=l_limit;i<=u_limit;i++)
+    {
+        if(noofDivisors(i)==num)
+        {
+            cout<<i<<endl;
+            count++;
+        }
+    }
+    cout<<num<<endl<<count;
+    return 0;
+}
--- Algorithm/perfect.cpp
+++ Algorithm/perfect.cpp
@@ -1,1 +1,24 @@
-
+#include<iostream>
+using namespace std;
+int main()
+{
+    int num,rem,sum=0,i;
+    cin>>num;
+    for(i=1;i<num;i++)
+    {
+        rem=num%i;
+        if(rem==0)
+        {
+            sum=sum+i;
+        }
+    }
+    if(sum==num)
+    {
+        cout<<"Perfect Number";
+    }
+    else
+    {
+        cout<<"Not Perfect Number";
+    }
+    return 0;
+}
