71382923e13950d39c0b147c3be056055d98f755
mohsin0176 <mohsin2010ju@gmail.com>
2021-01-04 19:47:49 +0600

    C++ Binary To octal codes

--- Algorithm/BtoO.cpp
+++ Algorithm/BtoO.cpp
@@ -1,1 +1,31 @@
+#include<iostream>
+#include<cmath>
+using namespace std;
+int convertBinaryToOcatl(long long);
+int convertBinaryToOcatl(long long binaryNumber)
 
+{
+    int octalNumber=0,decimalNumber=0,i=0;
+    while(binaryNumber !=0)
+    {
+      decimalNumber+=(binaryNumber%10)*pow(2,i);
+      i++;
+      binaryNumber/=10;
+    }
+    i=1;
+    while(decimalNumber!=0)
+    {
+        octalNumber+=(decimalNumber%8)*i;
+        decimalNumber/=8;
+        i*=10;
+    }
+    return octalNumber;
+}
+
+int main()
+{
+    long long binaryNumber;
+    cin>>binaryNumber;
+    cout<<convertBinaryToOcatl(binaryNumber);
+    return 0;
+}
