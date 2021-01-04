351b57c458b9fe308030e03630bc5d64547f6ed8
mohsin0176 <mohsin2010ju@gmail.com>
2021-01-04 19:36:20 +0600

    C++ Binary To Number codes

--- Algorithm/BtoD.cpp
+++ Algorithm/BtoD.cpp
@@ -1,1 +1,23 @@
-
+#include<iostream>
+#include<cmath>
+using namespace std;
+int convertBinaryTodecimal(long long);
+int main()
+{
+    long long n;
+    cin>>n;
+    cout<<convertBinaryTodecimal(n);
+    return 0;
+}
+int convertBinaryTodecimal(long long n)
+{
+    int decimalNumber=0,i=0,remainder;
+    while(n!=0)
+    {
+        remainder=n%10;
+        n/=10;
+        decimalNumber+=remainder*pow(2,i);
+        ++i;
+    }
+    return decimalNumber;
+}
