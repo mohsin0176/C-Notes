2bf8a34562504fcf9c527913f02aac14dbdab2a3
mohsin0176 <mohsin2010ju@gmail.com>
2021-01-04 22:06:24 +0600

    C++  codes

--- Algorithm/harshad.cpp
+++ Algorithm/harshad.cpp
@@ -1,1 +1,25 @@
 
+#include<iostream>
+using namespace std;
+int main()
+{
+    int num,sum=0;
+    cin>>num;
+    int n=num;
+    while(num>0)
+    {
+        int rem=num%10;
+        sum=sum+rem;
+        num=num/10;
+
+    }
+    if(n%sum==0)
+    {
+        cout<<n<<"   Harshad Number";
+    }
+    else
+    {
+        cout<<n<<"  Not Harshad Number";
+    }
+    return 0;
+}
