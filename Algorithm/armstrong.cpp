2052bb3f00a2a81832f7e3c766bff2db68d02672
mohsin0176 <mohsin2010ju@gmail.com>
2021-01-04 18:29:58 +0600

    C++ codes

--- Algorithm/armstrong.cpp
+++ Algorithm/armstrong.cpp
@@ -1,1 +1,24 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int num,originalNum,remainder,result=0;
+    cin>>num;
+    originalNum=num;
+    while(originalNum !=0)
+    {
+        remainder=originalNum%10;
+        result+=remainder*remainder*remainder;
+        originalNum/=10;
 
+    }
+    if(result==num)
+    {
+        cout<<num<<"  is Armstrong Number";
+    }
+    else
+    {
+        cout<<num<<"  is not Armstrong Number";
+    }
+    return 0;
+}
