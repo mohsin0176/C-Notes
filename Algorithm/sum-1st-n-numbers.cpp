26c0e9eb4a58e35573479423e1530c7daba7ea63
mohsin0176 <mohsin2010ju@gmail.com>
2021-01-04 20:19:54 +0600

    C++  codes

--- Algorithm/sum-1st-n-numbers.cpp
+++ Algorithm/sum-1st-n-numbers.cpp
@@ -1,1 +1,22 @@
 
+#include<iostream>
+using namespace std;
+int main()
+{
+    int sum=0,i,n,number,arr[100];
+    cout<<"How many Numbers you want to take as input:"<<endl;
+    cin>>n;
+     cout<<"How many Numbers you want to add:"<<endl;
+    cin>>number;
+    for(i=0;i<=(n-1);i++)
+    {
+        cin>>arr[i];
+    }
+        for(i=0;i<=(number-1);i++)
+    {
+        sum+=arr[i];
+    }
+     cout<<"Total Numbers after added:"<<endl;
+    cout<<sum;
+    return 0;
+}
