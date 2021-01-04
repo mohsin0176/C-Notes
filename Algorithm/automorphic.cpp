0f36fa1928ddfc727c0a58b814a170c014500208
mohsin0176 <mohsin2010ju@gmail.com>
2021-01-04 18:55:37 +0600

    C++ armstrong codes

--- Algorithm/automorphic.cpp
+++ Algorithm/automorphic.cpp
@@ -1,1 +1,29 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int num,flag=0;
+    cin>>num;
+    int sq=num*num;
+    int store=num;
+    while(num>0)
+    {
+        if(num%10!=sq%10)
+        {
+            flag=1;
+            break;
+        }
+        num=num/10;
+        sq=sq/10;
 
+    }
+    if(flag==1)
+    {
+        cout<<store<<"is not Automorphic number";
+    }
+    else
+    {
+        cout<<store<<"is  Automorphic number";
+    }
+    return 0;
+}
