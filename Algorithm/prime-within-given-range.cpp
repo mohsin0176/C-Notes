36a1d532acc664df39f4da0def48580cabb7bda6
mohsin0176 <mohsin2010ju@gmail.com>
2021-01-04 21:08:17 +0600

    C++  codes

--- Algorithm/prime-within-given-range.cpp
+++ Algorithm/prime-within-given-range.cpp
@@ -1,1 +1,36 @@
+#include<iostream>
+using namespace std;
 
+int main()
+{
+    int low,high,i;
+    bool isPrime=true;
+    cin>>low>>high;
+
+    while(low<high)
+    {
+        isPrime=true;
+        if(low==0 || low==1)
+        {
+            isPrime=false;
+        }
+        else
+        {
+            for(i=2;i<=low/2;i++)
+            {
+                if(low%i==0)
+                {
+                    isPrime=false;
+                    break;
+                }
+            }
+        }
+        if(isPrime)
+        {
+            cout<<low<<endl;
+
+        }
+          low++;
+    }
+    return 0;
+}
