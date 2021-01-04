31c3a7d41632738766b389af3da11945396924fa
mohsin0176 <mohsin2010ju@gmail.com>
2021-01-04 23:05:15 +0600

    C++  codes

--- Algorithm/rep-0-by-1.cpp
+++ Algorithm/rep-0-by-1.cpp
@@ -1,1 +1,23 @@
-
+#include<iostream>
+using namespace std;
+int main()
+{
+    int num,num2=0;
+    cin>>num;
+    if(num==0)
+    {
+        num2=1;
+    }
+    while(num>0)
+    {
+        int rem=num%10;
+        if(rem==0)
+        {
+            rem=1;
+        }
+        num/=10;
+        num2=num2*10+rem;
+    }
+    cout<<num2;
+    return 0;
+}
