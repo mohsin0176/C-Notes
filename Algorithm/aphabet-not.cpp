aaa7c9894dc9983d361b7d9c8c2463f20cdf6534
mohsin0176 <mohsin2010ju@gmail.com>
2021-01-04 18:23:02 +0600

    C++ codes

--- Algorithm/aphabet-not.cpp
+++ Algorithm/aphabet-not.cpp
@@ -1,1 +1,18 @@
 
+#include<iostream>
+using namespace std;
+int main()
+{
+
+    char c;
+    cin>>c;
+    if((c>='a' && c<='z')||(c>='A'&&c<='Z'))
+    {
+        cout<<"Alphabet";
+    }
+    else
+    {
+        cout<<"Not Alphabet";
+    }
+    return 0;
+}
