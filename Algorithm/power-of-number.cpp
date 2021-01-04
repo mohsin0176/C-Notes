73afe9f22c99efecdcc20a5fdfda288e956c54db
mohsin0176 <mohsin2010ju@gmail.com>
2021-01-04 21:15:38 +0600

    C++  codes

--- Algorithm/power-of-number.cpp
+++ Algorithm/power-of-number.cpp
@@ -1,1 +1,18 @@
+#include<iostream>
+using namespace std;
 
+int main()
+{
+
+    int base,result=1;
+    int power;
+    int i;
+    cin>>base;
+    cin>>power;
+    for(i=1;i<=power;i++)
+    {
+        result=result*base;
+    }
+    cout<<result;
+    return 0;
+}
