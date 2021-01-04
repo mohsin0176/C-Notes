7db26e635376faf8b919a6c2bcd71a1f2aa3f96f
mohsin0176 <mohsin2010ju@gmail.com>
2021-01-04 18:49:21 +0600

    C++ armstrong codes

--- Algorithm/armstrong-number-in-given-range.cpp
+++ Algorithm/armstrong-number-in-given-range.cpp
@@ -1,1 +1,29 @@
+#include<iostream>
+#include<math.h>
+using namespace std;
 
+int main()
+{
+    int num,sum,r,t,mm;
+    int sno,eno;
+    cin>>sno>>eno;
+    cout<<endl;
+    for(num=sno;num<=eno;num++)
+    {
+        t=num;
+        sum=0;
+        while(t!=0)
+        {
+            r=t%10;
+            mm=pow(r,3);
+            sum=sum+mm;
+            t=t/10;
+        }
+        if(sum==num)
+        {
+            cout<<num;
+        }
+    }
+    cout<<endl;
+    return 0;
+}
